7a17783dd92cc5f33868f1fce2078e1d2f7473dc
Diogo <a48302@alunos.isel.pt>
2021-11-16 14:49:47 +0000

    deleteGroup function

--- borga-data-mem.js
+++ borga-data-mem.js
@@ -11,3 +11,15 @@ const groups = {
 
 // GROUPS MANAGEMENT \\
 // CREATE, DELETE, EDIT(NAME,GAMES(DELETE, ADD, LIST_ALL))
+
+/**
+ * Delete a Games Group
+ * @param {group_ID} identifier of a group
+ * @returns true if group got deleted or false if group doesn't exist or couldn't be deleted 
+ */
+function deleteGroup(group_ID) {
+    if (!groupExits(group_ID)) return false
+    delete groups[group_ID]
+    // Make sure group got deleted
+    if (groupExits(group_ID)) return false; else return true
+}
