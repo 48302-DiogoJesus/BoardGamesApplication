03c790d6c272835bda79591d34145672a75f64b7
Diogo <a48302@alunos.isel.pt>
2021-11-16 14:33:26 +0000

    "borga-data-mem.js" first structure

--- borga-data-mem.js
+++ borga-data-mem.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const error = require('./borga-errors')
+
+const groups = {
+    1 : {
+        name : "Meu grupo",
+        games : ['aJHA87BH', 'asjkdh8GHSGD']
+    }
+}
+
+// GROUPS MANAGEMENT \\
+// CREATE, DELETE, EDIT(NAME,GAMES(DELETE, ADD, LIST_ALL))
