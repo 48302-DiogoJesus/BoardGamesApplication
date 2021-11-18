e309e36bb79bf2c10267b08e8628ccfc2ed70e91
djalo-sd <jmgilmenezes17@gmail.com>
2021-11-18 20:33:01 +0000

    noice

--- borga-services.js
+++ borga-services.js
@@ -15,11 +15,6 @@ module.exports = function (games_data, data_mem) {
     	createGroup : data_mem.createGroup,
 	
     	deleteGroup : data_mem.deleteGroup,
-<<<<<<< HEAD
-=======
-    	getGroup : data_mem.getGroup,
-		getGroups : data_mem.getGroups,
->>>>>>> 9e6c53c50c04ec83cbf074e406b4af310259a584
     	deleteGroupGame : data_mem.deleteGroupGame,
     	addGroupGame : data_mem.addGroupGame,
 
