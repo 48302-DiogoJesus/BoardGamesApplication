62d03303c40f2fdc3808dbeb7c9b6dc54024ef55
Diogo Jesus <a48302@alunos.isel.pt>
2021-11-20 15:26:16 +0000

    Added all http error codes and general information about them

--- borga-errors.js
+++ borga-errors.js
@@ -1,5 +1,26 @@
 'use strict';
 
+/*
+* HTTP COMMON ERROR CODES AND MEANING *
+1xx Information
+2xx Success
+3xx Redirection
+4xx Client Error
+5xx Server Error
+
+200 - OK
+201 - RESOURCE CREATED
+204 - RESOURCE UPDATED (DELETED TOO)
+
+400 - BAD REQUEST
+403 - FORBIDDEN
+404 - NOT FOUND
+409 - CONFLICT (RESOURCE ALREADY EXISTS)
+500 - INTERNAL SERVER ERROR
+502 - BAD GATEWAY
+503 - SERVICE UNAVAILABLE
+*/
+
 // All known errors are contained inside this object
 module.exports = {
     GLOBAL_UNKNOWN_ERROR: (info) => {
@@ -11,10 +32,12 @@ module.exports = {
     },
     BGATLAS_UNAVAILABLE: {
         code: 101,
+        http_code: 503,
         message: 'Board Game Altas API is currently offline'
     },
     BGATLAS_UNEXPECTED_RESPONSE: {
         code: 102,
+        http_code: 500,
         message: 'Board Game Atlas API gave an unexpected response'
     },
     BGATLAS_NOT_FOUND: {
@@ -29,10 +52,12 @@ module.exports = {
     },
     DATA_MEM_GROUP_ALREADY_EXISTS: {
         code: 202,
+        http_code: 409,
         message: 'Group already exists'
     },
     DATA_MEM_GROUP_NOT_DELETED: {
         code: 203,
+        http_code: 500,
         message: 'Group could not be deleted'
     },
     DATA_MEM_GROUP_DOES_NOT_HAVE_GAME: {
@@ -42,34 +67,42 @@ module.exports = {
     },
     DATA_MEM_GAME_NOT_DELETED_FROM_GROUP: {
         code: 205,
+        http_code: 500,
         message: 'Game could not be deleted from group' 
     },
     DATA_MEM_GROUP_ALREADY_HAS_GAME: {
         code: 206,
+        http_code: 409,
         message: 'Game is already inside that group'
     },
     DATA_MEM_INVALID_GROUP_NAME: {
         code: 207,
+        http_code: 400,
         message: 'Invalid group name'
     },
     DATA_MEM_COULD_NOT_ADD_GAME_TO_GROUP: {
         code: 208,
+        http_code: 500,
         message: 'Although group exists and this game is not a duplicate, could not add it to group'
     },
     DATA_MEM_INVALID_GROUP_DESCRIPTION : {
         code: 209,
+        http_code: 400,
         message: 'Invalid group description'
     },
     DATA_MEM_COULD_NOT_CREATE_GROUP : {
         code: 210,
+        http_code: 500,
         message: 'Could not create group'
     },
     DATA_MEM_INVALID_USERNAME : {
         code: 211,
+        http_code: 400,
         message: 'Invalid user name'
     },
     DATA_MEM_COULD_NOT_CREATE_USER : {
         code: 212,
+        http_code: 500,
         message: 'Could not create user'
     },
     DATA_MEM_USER_DOES_NOT_EXIST: {
@@ -79,22 +112,27 @@ module.exports = {
     },
     DATA_MEM_USER_COULD_NOT_BE_DELETED: {
         code: 214,
+        htpp_code: 500,
         message: 'User could not be deleted'
     },
     DATA_MEM_USER_ALREADY_HAS_THIS_GROUP: {
         code: 215,
+        http_code: 409,
         message: 'Group already inside this user groups'
     },
     DATA_MEM_COULD_NOT_ADD_GROUP_TO_USER: {
         code: 216,
+        http_code: 500,
         message: 'Could not add group to user'
     },
     DATA_MEM_USER_DOES_NOT_HAVE_THIS_GROUP: {
         code: 217,
+        http_code: 404,
         message: 'This user does not have that group'
     },
     DATA_MEM_GROUP_NOT_DELETED_FROM_USER: {
         code: 218,
+        http_code: 500,
         message: 'Group was not deleted from user groups'
     },
     WEB_API_INVALID_QUERY: {
@@ -104,14 +142,17 @@ module.exports = {
     },
     WEB_API_INVALID_GROUP_DETAILS: {
         code: 302,
+        http_code: 400, 
         message: 'Invalid group details'
     },
     WEB_API_INSUFICIENT_GROUP_INFORMATION: {
         code: 303,
+        http_code: 400,
         messge: 'Insuficient group information'
     },
     WEB_API_INVALID_USER_NAME: {
         code: 304,
+        http_code: 400,
         message: 'Invalid user name'
     }
 };
